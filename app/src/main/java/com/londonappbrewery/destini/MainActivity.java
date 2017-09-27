cde5a1ae1a0677901448d508d2945c2d2cb5c966
Kasra Pourang <kpourang@nd.edu>
2017-09-26 22:57:48 -0400

    Done!

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,21 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
+
+
+    Button buttonTop;
+    Button buttonBottom;
+    TextView storyTextView;
+    int mStoryIndex = 1;
+
 
 
     @Override
@@ -13,18 +24,84 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
+        buttonBottom = (Button) findViewById(R.id.buttonBottom);
+        buttonTop = (Button) findViewById(R.id.buttonTop);
+        storyTextView = (TextView) findViewById(R.id.storyTextView);
+        storyTextView.setText(R.string.T1_Story);
+        buttonBottom.setText(R.string.T1_Ans2);
+        buttonTop.setText(R.string.T1_Ans1);
 
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        buttonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storyTextView.setText(R.string.T2_Story);
+                buttonBottom.setText(R.string.T2_Ans2);
+                buttonTop.setText(R.string.T2_Ans1);
+                buttonTop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        storyTextView.setText(R.string.T3_Story);
+                        buttonBottom.setText(R.string.T3_Ans2);
+                        buttonTop.setText(R.string.T3_Ans1);
+                        buttonBottom.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                storyTextView.setText(R.string.T5_End);
+                                buttonBottom.setVisibility(View.GONE);
+                                buttonTop.setVisibility(View.GONE);
+                            }
+                        });
+                        buttonTop.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                storyTextView.setText(R.string.T6_End);
+                                buttonBottom.setVisibility(View.GONE);
+                                buttonTop.setVisibility(View.GONE);
+
+                            }
+                        });
+                    }
+                });
+                buttonBottom.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        storyTextView.setText(R.string.T4_End);
+                        buttonBottom.setVisibility(View.GONE);
+                        buttonTop.setVisibility(View.GONE);
+                    }
+                });
+            }
+
+        });
 
 
+
+
+        buttonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                storyTextView.setText(R.string.T3_Story);
+                buttonBottom.setText(R.string.T3_Ans2);
+                buttonTop.setText(R.string.T3_Ans1);
+                buttonTop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        storyTextView.setText(R.string.T6_End);
+                        buttonBottom.setVisibility(View.GONE);
+                        buttonTop.setVisibility(View.GONE);
+                    }
+                });
+                buttonBottom.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        storyTextView.setText(R.string.T5_End);
+                        buttonBottom.setVisibility(View.GONE);
+                        buttonTop.setVisibility(View.GONE);
+                    }
+                });
+
+            }
+        });
     }
 }
